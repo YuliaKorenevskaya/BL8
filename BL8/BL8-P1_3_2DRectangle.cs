2dea36b3e60ce63a3ff93d0c5cbaa9f33a35056d
tegami2007@mail.ru <tegami2007@mail.ru>
2019-04-25 22:58:28 +0300

    Добавьте файлы проекта.

---
+++ BL8/BL8-P1_3_2DRectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL_8
+{
+    class Program
+    {
+       static void Main(string[] args)
+      {
+
+      }
+
+        public struct Rectangle
+         {
+            public int height;
+            public int width;
+            public int x;
+            public int y;
+
+            public Rectangle (int height1, int width1, int x1, int y1)
+            {
+                height = height1;
+                width = width1;
+                x = x1;
+                y = y1;
+            }
+         }
+        
+    }
+}
---
+++ BL8/BL8-P2_3_2DRectangleMassive.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL_8
+{
+    class Program
+    {
+        public struct Square
+        {
+            public int side;            
+        }
+        static void Main(string[] args)
+        {
+            Square[] group = new Square[100];
+            int[] mas = new int[100];
+            Random r = new Random();
+            int count = 1;
+            int j = 0;
+
+            for (int i = 0; i < 100; i++)
+            { 
+                group[i].side =  r.Next(1,11);
+                mas[j] = group[i].side;              
+                Console.Write($"{group[i].side} ");
+                j++;
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (j = 0; j < 100; j++)
+                {
+                    if (group[i].side == mas[j])
+                    {                     
+                        count++;
+                        mas[j] = 0;
+                    }
+                } 
+
+                if (count > 1)
+                {
+                    Console.WriteLine($"Квадрат со стороной - {group[i].side}; повторов - {count} ");
+                }
+               
+                count = 0;           
+            }
+       
+            Console.ReadKey();
+        }
+    }
+}
